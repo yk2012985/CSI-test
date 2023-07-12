e5e39c02737d0c49b5ecadf087b9cd7042eab59d
yk2012985 <445001186@qq.com>
2023-07-12 10:35:23 +0800

    Add GetPluginInfo of identityfyserver.

--- pkg/driver/identifyserver.go
+++ pkg/driver/identifyserver.go
@@ -1,7 +1,19 @@
 package driver
 
-import csicommon "github.com/kubernetes-csi/drivers/pkg/csi-common"
+import (
+	"context"
+	"github.com/container-storage-interface/spec/lib/go/csi"
+	csicommon "github.com/kubernetes-csi/drivers/pkg/csi-common"
+)
 
 type identifyServer struct {
 	*csicommon.DefaultIdentityServer
+}
+
+func (d *identifyServer) GetPluginInfo(ctx context.Context, req *csi.GetPluginInfoRequest) (*csi.GetPluginInfoResponse, error) {
+	resp := &csi.GetPluginInfoResponse{
+		Name:          driverName,
+		VendorVersion: vendorVersion,
+	}
+	return resp, nil
 }
